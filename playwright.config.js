32c6c374095dba441ae0c8019599105fe443591f
Mahalaxmi <mahalaxmisagarla@gmail.com>
2026-05-29 12:46:16 +0530

    Set retries to 1 always; auto-clean allure-results before test run

--- playwright.config.js
+++ playwright.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 1 : 0,
+  retries: 1,
   workers: 1,
   reporter: [
     ['html'],
